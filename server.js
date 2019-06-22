e0b317cdfdcab9b6073182ad507844e2a8a57aaa
ssslavev <s.s.slavev@hotmail.com>
2019-06-22 21:58:45 +0300

    Initial commit with socket.io implementation

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+const server = app.listen(port, () => {
+    console.log('Server is running on port: ' + port);
+});
+
+
+const io = socket(server, { pingTimeout: 60000 });
+
+
+io.on('connection', (socket) => {
+
+    console.log("Socket connection was maded!");
+
+    socket.on('join', (data) => {
+        socket.join(data.id);
+
+    });
+
+
+
+    socket.on("sendMessage", function (message) {
+        console.log("Message received:");
+        console.log(message);
+
+
+        io.to(message.toId).emit("messageReceived", message);
+
+        console.log("Message dispatched.");
+    });
+
+})
+
